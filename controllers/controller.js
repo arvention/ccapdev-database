f5ea38995ec200f44955063ab7bdef2d40ffc0da
Arren Matthew C. Antioquia <amc.antioquia@yahoo.com.ph>
2020-03-28 14:05:54 +0800

    updated controller.js

--- controllers/controller.js
+++ controllers/controller.js
@@ -18,20 +18,26 @@ const controller = {
         // gets the parameter `username` from the URL
         var u = req.params.username;
 
-        // creates an object `query` which assigns the value of the variable `u` to field `username`
+        // creates an object `query`
+        // which assigns the value of the variable `u` to field `username`
         var query = {username: u};
 
-        // calls the function findOne() defined in the `database` object in `../models/db.js`
-        // this function searches the collection `profiles` based on the value set in object `query`
+        // calls the function findOne()
+        // defined in the `database` object in `../models/db.js`
+        // this function searches the collection `profiles`
+        // based on the value set in object `query`
         // the third parameter is a callback function
-        // this called when the database returns a value saved in variable `result`
+        // this called when the database returns a value
+        // saved in variable `result`
         db.findOne('profiles', query, function (result) {
 
-            // renders `../views/profile.hbs` with the values in variable `results`
+            // renders `../views/profile.hbs`
+            // with the values in variable `results`
             res.render('profile', result);
         });
     }
 }
 
-// exports the object `controller` (defined above) when another script exports from this file
+// exports the object `controller` (defined above)
+// when another script exports from this file
 module.exports = controller;
