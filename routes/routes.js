4caf4742b60ae7451a4bb9924f4d9ffff754c112
Arren Matthew C. Antioquia <amc.antioquia@yahoo.com.ph>
2020-03-28 14:03:09 +0800

    updated routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -2,18 +2,22 @@
 // import module `express`
 const express = require('express');
 
-// import module controller from `../controllers/controller.js`
+// import module `controller` from `../controllers/controller.js`
 const controller = require('../controllers/controller.js')
 
 const app = express();
 
-// execute function getFavicon() as defined in object controller in `../controllers/controller.js`
+// execute function getFavicon()
+// defined in object `controller` in `../controllers/controller.js`
 // when a client sends an HTTP GET request for `/favicon.ico`
 app.get('/favicon.ico', controller.getFavicon);
 
-// execute function getFavicon() as defined in object controller in `../controllers/controller.js`
-// when a client sends an HTTP GET request for `/:username` where `username` is a parameter
+// execute function getFavicon()
+// defined in object `controller` in `../controllers/controller.js`
+// when a client sends an HTTP GET request for `/:username`
+// where `username` is a parameter
 app.get('/:username', controller.getProfile);
 
-// exports the object `app` (defined above) when another script exports from this file
+// exports the object `app` (defined above)
+// when another script exports from this file
 module.exports = app;
